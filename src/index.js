cf13f02e2be821392b7c8106dca02132fcd4aea6
Lotta <lotsulotsu@gmail.com>
2026-06-08 19:17:55 +0300

    setting up the webpack

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
-// index.js
+// src/index.js
+import "./styles.css";
 import { greeting } from "./greeting.js";
 
 console.log(greeting);
+// src/index.js
+import odinImage from "./test.jpg";
+
+const image = document.createElement("img");
+image.src = odinImage;
+
+document.body.appendChild(image);
