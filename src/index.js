7fe285948fa48f804cd4f9ec89d0c3c2cd61c2eb
Lotta <lotsulotsu@gmail.com>
2026-06-08 19:46:07 +0300

    begin with the html structure for the main content

--- src/index.js
+++ src/index.js
@@ -3,10 +3,3 @@ import "./styles.css";
 import { greeting } from "./greeting.js";
 
 console.log(greeting);
-// src/index.js
-import odinImage from "./test.jpg";
-
-const image = document.createElement("img");
-image.src = odinImage;
-
-document.body.appendChild(image);
